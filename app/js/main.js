d4c1d024d096e6860e85740f9e1f92ebf2c848fa
Krasnov91 <krasnoshtanov10.11.91@gmail.com>
2024-09-08 18:17:26 +0200

    added scrolling menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,11 @@
-$(function(){
+$(function () {
+
+  $(".header__link, .logo").on("click", function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 800);
+  });
 
   $(window).scroll(function () {
     var sticky = $('.header__inner'),
