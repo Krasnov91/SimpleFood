cf3a767f0a265ea6e291dc6c9008f91105cd0da7
Krasnov91 <krasnoshtanov10.11.91@gmail.com>
2024-09-08 16:44:50 +0200

    added mixitup, fixed header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function(){
 
+  $(window).scroll(function () {
+    var sticky = $('.header__inner'),
+      scroll = $(window).scrollTop();
+
+    if (scroll >= 10) sticky.addClass('header__inner-fixed');
+    else sticky.removeClass('header__inner-fixed');
+  });
+
 });
+
+var mixer = mixitup('.category__inner');
