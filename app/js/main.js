066ab7ffe6eac6e62b3c0a1196f20e1dc47a0924
Krasnov91 <krasnoshtanov10.11.91@gmail.com>
2024-09-17 15:49:11 +0200

    adaptiv version

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,16 @@ $(function () {
 
   $('.burger-menu, .menu__close').on('click', function () {
     $('.menu__mobile').toggleClass('menu__mobile--active');
+    $('.burger-menu').toggleClass('burger-menu--active');
     $('body').toggleClass('lock');
+  });
+
+  document.addEventListener('click', function (e) {
+    if (e.target !== burger && e.target !== mobileMenu) {
+      burger.classList.remove('burger-menu--active');
+      mobileMenu.classList.remove('menu__mobile--active');
+      bodyLock.classList.remove('lock');
+    }
   });
 
   $(".header__link, .logo").on("click", function (e) {
@@ -24,14 +33,26 @@ $(function () {
     dots: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-
+    infinite: false,
     arrows: true,
     speed: 1000,
 
     prevArrow: '<button type="button" class="slick-arrow slick-prev"><svg class="slick-arrow__svg"><use xlink:href="images/sprite.svg#icon-arrow"></use></svg></button>',
     nextArrow: '<button type="button" class="slick-arrow slick-next"><svg class="slick-arrow__svg"><use xlink:href="images/sprite.svg#icon-arrow"></use></svg></button>',
-    // appendArrows: '.arrows-wrap'
+  });
 
+  $(window).on('load resize', function () {
+    if ($(window).width() < 768) {
+      $('.restaurants__wrap:not(.slick-initialized)').slick({
+        arrows: false,
+        dots:true,
+        infinite:true,
+        speed:100,
+        slidesToShow: 1
+      });
+    } else {
+      $(".restaurants__wrap.slick-initialized").slick("unslick");
+    }
   });
 
 });
